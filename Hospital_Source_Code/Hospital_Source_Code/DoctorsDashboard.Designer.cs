daabd301ecde1515fde504205560d5a079dcba31
Andrew Byrne <Andybyrne23@hotmail.com>
2020-02-25 15:29:44 +0000

    Report

--- Hospital_Source_Code/Hospital_Source_Code/DoctorsDashboard.Designer.cs
+++ Hospital_Source_Code/Hospital_Source_Code/DoctorsDashboard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoctorsDashboard));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabDoctors = new System.Windows.Forms.TabPage();
             this.lblDocIdlabel = new System.Windows.Forms.Label();
@@ -55,6 +56,9 @@
             this.txtSearchDoc1 = new System.Windows.Forms.TextBox();
             this.btnSearchDoc = new System.Windows.Forms.Button();
             this.btnDocReport = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.tabControl1.SuspendLayout();
             this.tabDoctors.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -379,6 +383,25 @@
             this.btnDocReport.Text = "REPORT";
             this.btnDocReport.UseVisualStyleBackColor = true;
             this.btnDocReport.Visible = false;
+            this.btnDocReport.Click += new System.EventHandler(this.btnDocReport_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage_1);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
             // 
             // DoctorsDashboard
             // 
@@ -427,5 +450,8 @@
         private System.Windows.Forms.TextBox txtDocLastName;
         private System.Windows.Forms.TextBox txtDocFirstName;
         private System.Windows.Forms.Button btnDocReport;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
